bb5ee5babfbb38ae06efedb040eef19586dbe5de
Julian Nonino <noninojulian@gmail.com>
2019-01-28 16:20:52 -0300

    Adding description to outputs

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,7 @@ output "vpc_cidr_block"{
 # AWS Internet Gateway
 # ---------------------------------------------------------------------------------------------------------------------
 output "internet_gateway_id" {
+    description = "ID of the generated Internet Gateway"
     value = "${aws_internet_gateway.internet_gw.id}"
 }
 
@@ -21,12 +22,15 @@ output "internet_gateway_id" {
 # AWS Subnets - Public
 # ---------------------------------------------------------------------------------------------------------------------
 output "public_subnets_ids" {
+    description = "List with the Public Subnets IDs"
     value = [ "${aws_subnet.public_subnets.*.id}"]
 }
 output "public_subnets_route_table_id" {
+    description = "ID of the Route Table used on Public networks"
     value = "${aws_route_table.public_subnets_route_table.id}"
 }
 output "nat_gw_ids" {
+    description = "List with the IDs of the NAT Gateways created on public subnets to provide internet to private subnets"
     value = "${aws_nat_gateway.nat_gw.*.id}"
 }
 
@@ -34,8 +38,10 @@ output "nat_gw_ids" {
 # AWS Subnets - Private
 # ---------------------------------------------------------------------------------------------------------------------
 output "private_subnets_ids" {
+    description = "List with the Private Subnets IDs"
     value = ["${aws_subnet.private_subnets.*.id}"]
 }
 output "private_subnets_route_table_id" {
+    description = "ID of the Route Table used on Private networks"
     value = "${aws_route_table.private_subnets_route_table.id}"
 }
