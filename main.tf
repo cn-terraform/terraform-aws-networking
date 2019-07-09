9373ac681bd90291374c3297f3367a93aa749fd7
Julian Nonino <noninojulian@gmail.com>
2019-07-09 14:13:26 +0100

    Updating to make it suitable for Terraform 0.12

--- main.tf
+++ main.tf
@@ -2,28 +2,29 @@
 # PROVIDER
 # ---------------------------------------------------------------------------------------------------------------------
 provider "aws" {
-  profile   = "${var.profile}"
-  region    = "${var.region}"
+  profile = var.profile
+  region  = var.region
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS Virtual Private Network
 # ---------------------------------------------------------------------------------------------------------------------
 resource "aws_vpc" "vpc" {
-    cidr_block           = "${var.vpc_cidr_block}" # The CIDR block for the VPC.
-    enable_dns_support   = true # A boolean flag to enable/disable DNS support in the VPC.
-    enable_dns_hostnames = true # A boolean flag to enable/disable DNS hostnames in the VPC.
-    tags {
-        Name = "${var.name_preffix}-vpc"
-    }
+  cidr_block           = var.vpc_cidr_block # The CIDR block for the VPC.
+  enable_dns_support   = true               # A boolean flag to enable/disable DNS support in the VPC.
+  enable_dns_hostnames = true               # A boolean flag to enable/disable DNS hostnames in the VPC.
+  tags = {
+    Name = "${var.name_preffix}-vpc"
+  }
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS Internet Gateway
 # ---------------------------------------------------------------------------------------------------------------------
 resource "aws_internet_gateway" "internet_gw" {
-    vpc_id = "${aws_vpc.vpc.id}"
-    tags {
-        Name = "${var.name_preffix}-internet-gw"
-    }
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.name_preffix}-internet-gw"
+  }
 }
+
--- outputs.tf
+++ outputs.tf
@@ -2,46 +2,51 @@
 # VPC
 # ---------------------------------------------------------------------------------------------------------------------
 output "vpc_id" {
-    description = "The ID of the VPC"
-    value       = "${aws_vpc.vpc.id}"
+  description = "The ID of the VPC"
+  value       = aws_vpc.vpc.id
 }
-output "vpc_cidr_block"{
-    description = "The CIDR block of the VPC"
-    value       = "${aws_vpc.vpc.cidr_block}"
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = aws_vpc.vpc.cidr_block
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS Internet Gateway
 # ---------------------------------------------------------------------------------------------------------------------
 output "internet_gateway_id" {
-    description = "ID of the generated Internet Gateway"
-    value = "${aws_internet_gateway.internet_gw.id}"
+  description = "ID of the generated Internet Gateway"
+  value       = aws_internet_gateway.internet_gw.id
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS Subnets - Public
 # ---------------------------------------------------------------------------------------------------------------------
 output "public_subnets_ids" {
-    description = "List with the Public Subnets IDs"
-    value = [ "${aws_subnet.public_subnets.*.id}" ]
+  description = "List with the Public Subnets IDs"
+  value       = [aws_subnet.public_subnets.*.id]
 }
+
 output "public_subnets_route_table_id" {
-    description = "ID of the Route Table used on Public networks"
-    value = "${aws_route_table.public_subnets_route_table.id}"
+  description = "ID of the Route Table used on Public networks"
+  value       = aws_route_table.public_subnets_route_table.id
 }
+
 output "nat_gw_ids" {
-    description = "List with the IDs of the NAT Gateways created on public subnets to provide internet to private subnets"
-    value = "${aws_nat_gateway.nat_gw.*.id}"
+  description = "List with the IDs of the NAT Gateways created on public subnets to provide internet to private subnets"
+  value       = aws_nat_gateway.nat_gw.*.id
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS Subnets - Private
 # ---------------------------------------------------------------------------------------------------------------------
 output "private_subnets_ids" {
-    description = "List with the Private Subnets IDs"
-    value = [ "${aws_subnet.private_subnets.*.id}" ]
+  description = "List with the Private Subnets IDs"
+  value       = [aws_subnet.private_subnets.*.id]
 }
+
 output "private_subnets_route_table_id" {
-    description = "ID of the Route Table used on Private networks"
-    value = "${aws_route_table.private_subnets_route_table.id}"
+  description = "ID of the Route Table used on Private networks"
+  value       = aws_route_table.private_subnets_route_table.id
 }
+
