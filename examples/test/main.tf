51d46192a7681ceef61636473ae19c656d2f7348
Julian Nonino <noninojulian@gmail.com>
2020-05-05 14:43:26 +0100

    Fix test module

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -3,6 +3,10 @@ provider "aws" {
 }
 
 module "cluster" {
-  source = "../../"
-  name   = "test-cluster"
+  source                                      = "../../"
+  name_preffix                                = var.name_preffix
+  vpc_cidr_block                              = var.vpc_cidr_block
+  availability_zones                          = var.availability_zones
+  public_subnets_cidrs_per_availability_zone  = var.public_subnets_cidrs_per_availability_zone
+  private_subnets_cidrs_per_availability_zone = var.private_subnets_cidrs_per_availability_zone
 }
