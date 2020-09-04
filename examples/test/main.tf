5a404f48e0e8cc23620b4fa1291a23e830aa32ae
Julian Nonino <noninojulian@gmail.com>
2020-09-04 14:20:32 +0100

    Fixing issues

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "base-network" {
   source                                      = "../../"
-  name_prefix                                = "base"
+  name_prefix                                 = "base"
   vpc_cidr_block                              = "192.168.0.0/16"
   availability_zones                          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
   public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
