fd90e481dae65c80f60ca9268522dab410d55a3e
Mohammed Yahya <my.almusaddar@gmail.com>
2020-09-05 09:43:20 +0300

    add pre-commit

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -9,11 +9,14 @@ module "base-network" {
   vpc_cidr_block = "192.168.0.0/16"
   availability_zones = [
     "us-east-1a",
-  "us-east-1b"]
+    "us-east-1b"
+  ]
   public_subnets_cidrs_per_availability_zone = [
     "192.168.0.0/19",
-  "192.168.32.0/19"]
+    "192.168.32.0/19"
+  ]
   private_subnets_cidrs_per_availability_zone = [
     "192.168.128.0/19",
-  "192.168.160.0/19"]
+    "192.168.160.0/19"
+  ]
 }
