e72ce386ad0ced601e1e4794f2cc8dc98ff907f9
Julian Nonino <jnonino@users.noreply.github.com>
2025-06-23 16:21:42 +0100

    Fix deprecation

--- examples/cidr-from-aws-ipam/main.tf
+++ examples/cidr-from-aws-ipam/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc_ipam" "test" {
 resource "aws_vpc_ipam_pool" "test" {
   address_family = "ipv4"
   ipam_scope_id  = aws_vpc_ipam.test.private_default_scope_id
-  locale         = data.aws_region.current.name
+  locale         = data.aws_region.current.region
 }
 
 resource "aws_vpc_ipam_pool_cidr" "test" {
